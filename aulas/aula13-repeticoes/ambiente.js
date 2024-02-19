8c3c4faa1bd5456c87d32c0369ba73001a292206
Gui-GitHub <guilhermecavalcante222@gmail.com>
2024-02-19 13:04:10 -0300

    Create ambiente.js

---
+++ aulas/aula13-repeticoes/ambiente.js
@@ -0,0 +1,14 @@
+var c = 1
+do{
+    console.log(`PASSO ${c}`)
+    c++
+} while (c <= 6)
+
+/*var c = 1
+while (c <= 30) {
+    console.log(`PASSO ${c}`)
+    c++
+}
+*/
+
+//console.log('Tudo bem?')
